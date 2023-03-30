ed32a762e7d166a2650e699cfa9f0a7676b125d1
krnvst <stanislav.k@unitedtech.ai>
2023-03-31 01:31:56 +0300

    fix

--- src/components/Map/AlarmMapGenerate.vue
+++ src/components/Map/AlarmMapGenerate.vue
@@ -3,10 +3,10 @@
     <svg viewBox="0 0 1000 670" fill=green xmlns="http://www.w3.org/2000/svg">
         <g id="map">
             <MapRegion
-                v-for='region in regions'
-                :key='region'
-                :region=region
-                :api-states="states.find(states => states.id === region.id)"
+              v-for='region in regions'
+              :key='region'
+              :region=region
+              :api-states="states.find(states => states.id === region.id)"
             />
         </g>
     </svg>
